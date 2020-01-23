eefb503e5e186beb89b1a3398c977db1415c3ac4
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-23 20:30:07 +0900

    Offline Apollo Configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './globalStyles';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
+import GlobalStyle from './globalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<GlobalStyle />
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+);
